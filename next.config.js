ac52e8ab089a8f43efcefd76d5401c7abf3124d0
OLOWONISHAYE SUNKANMI <skprojectx12@gmail.com>
2025-12-14 12:57:37 -0800

    ui changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  output: 'export',
-  trailingSlash: true
+  output: "export",
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
